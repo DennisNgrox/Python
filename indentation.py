4b912b7f91e674af8b087284649a16ce010b3ef7
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-07-02 19:57:33 -0300

    Create indentation.py
    Estudo de Python sobre identação

---
+++ indentation.py
@@ -0,0 +1,20 @@
+import time
+
+
+def PensarPor10Segundos():
+    print('pensando')
+    time.sleep(10)
+    print('Lembrei!')
+
+
+if 10 > 5:
+    print('10 é maior que 5')
+
+
+class BemVindo():
+    def __init__(self):
+        print('Bem vindo')
+
+
+oi = BemVindo()
+
