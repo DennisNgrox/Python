162986053c2a5af30219372de1bf1f2b57327b58
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-07-06 19:14:42 -0300

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,21 @@
+carro = ['1', '2', '3']
+
+# Inserir
+
+carro.insert(3, '4')
+print(carro)
+
+# Remover
+
+carro.remove('4')
+print(carro)
+
+
+# Remover
+carro.pop(0)
+print(carro)
+
+
+# Alterar Listas
+carro[1] = 5
+print(carro)
