6e80ef620f5446bb8ca422a8fc9b521f5c77d3f7
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-07-12 19:04:26 -0300

    Create moeda.py

---
+++ exercicio_modulos/moeda.py
@@ -0,0 +1,15 @@
+def aumentar(valor, percentagem):
+    s = valor + valor / percentagem
+    return s
+    
+def diminuir(valor, menos):
+    s = valor - valor / menos
+    return s
+
+def dobro(valor):
+    s = valor * 2
+    return s
+
+def metade(valor):
+    s = valor / 2
+    return s
