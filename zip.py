230a11ce781a00abc52f8b24c59de98b35446e39
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-07-16 18:49:55 -0300

    Create zip.py

---
+++ zip.py
@@ -0,0 +1,10 @@
+lista = [1, 2, 3]
+lista2 = [4, 5, 6]
+
+zip1 = list(zip(lista, lista2, 'abc'))
+
+print(zip1)
+
+# Concatena lista com lista2 com abc
+
+# output: [(1, 4, 'a'), (2, 5, 'b'), (3, 6, 'c')]
