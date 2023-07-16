e334b180ac7d5743255b52b5076d5274cc40cdc1
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-07-16 18:42:20 -0300

    Create min_max_len_sum.py

---
+++ min_max_len_sum.py
@@ -0,0 +1,6 @@
+lista = (1, 2, 3, 4, 5)
+
+print(len(lista)) # Quantos valores tem nessa lista: output: 5
+print(min(lista)) # Menor valor dessa lista: output: 1
+print(max(lista)) # Maior valor dessa lista: output: 5
+print(sum(lista)) # Soma total de todos valores dessa lista: output: 15
