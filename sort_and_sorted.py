40a6250c33ee457dc3c5946e022560df3a62a82d
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-07-15 23:47:44 -0300

    Update and rename sorted.py to sort_and_sorted.py

--- sort_and_sorted.py
+++ sort_and_sorted.py
@@ -1,4 +1,5 @@
 lista = [1, 8, 7, 6, 2, 5]
+lista.sort()
 
 print(sorted(lista))
 
@@ -7,4 +8,10 @@ tupla = (1, 8, 7, 6, 2, 5)
 print(sorted(tupla))
 
 
+
 # Realizar a organização de valor do menor para maior
+
+print(lista)
+
+#o sorted não modifica a atual mas sim gera uma nova
+#o sort modifica a lista atual
